9c393be4753cabf0c038345233b40e68a0b59e68
simon <smtstrie@avans.nl>
2020-03-23 12:36:04 -0700

    Initial Commit

---
+++ ios3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios3
+//
+//  Created by Simon on 23/03/2020.
+//  Copyright © 2020 Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
